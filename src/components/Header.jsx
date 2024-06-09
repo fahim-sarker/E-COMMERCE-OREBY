ad947bfcdd227703cdd7d14fb0c7a0dd9277a74e
fahim-sarker <157319749+fahim-sarker@users.noreply.github.com>
2024-06-09 16:59:03 +0600

    demo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,35 +7,45 @@ import { Link } from 'react-router-dom';
 
 const Header = () => {
     let [show, setShow] = useState(false)
+    let menulist = useRef()
+    useEffect(() => {
+        document.addEventListener("click", (e) => {
+            if (menulist.current.contains(e.target) == true) {
+                setShow(!show)
+            } else {
+                setShow(false)
+            }
+        })
+    }, [show])
 
 
     return (
         <>
             <header className='lg:px-0 px-[20px]'>
-            <Container>
-                <div className="flex py-[32px] justify-between bg-[#ffff]">
-                    <div className="w-1/4">
-                        <img src={Lg} alt="logo" />
-                    </div>
+                <Container>
+                    <div className="flex py-[32px] justify-between bg-[#ffff]">
+                        <div className="w-1/4">
+                            <img src={Lg} alt="logo" />
+                        </div>
 
-                    <div className=" w-3/4 z-50">
-                        <ul className={`lg:flex gap-x-[40px] lg:justify-center pt-[30px] lg:pt-[0] lg:static text-center font-sans] text-[20px] absolute  duration-700 ease-out ${show == true ?"bg-[#262626] w-[60%]  top-[50px] left-[0px] h-full" : "bg-[] top-[50px] left-[-100px]"}`}>
-                            <li className='py-[5px] lg:py-[0px] font-bold  lg:hover:text-[#262626] lg:text-[#767676] duration-300 ease-in-out text-white hover:text-[gray]'>
-                                <Link to="/">Home</Link>
-                            </li>
-                            <li className='py-[10px] lg:py-[0px] font-bold  lg:hover:text-[#262626] lg:text-[#767676] duration-300 ease-in-out text-white hover:text-[gray]'>
-                                <Link to="/product">shop</Link>
-                            </li>
-                            <li className='py-[10px] lg:py-[0px] font-bold  lg:hover:text-[#262626] lg:text-[#767676] duration-300 ease-in-out text-white hover:text-[gray]'>About</li>
-                            <li className='py-[10px] lg:py-[0px] font-bold  lg:hover:text-[#262626] lg:text-[#767676] duration-300 ease-in-out text-white hover:text-[gray]'>Contacts</li>
-                            <li className='py-[10px] lg:py-[0px] font-bold  lg:hover:text-[#262626] lg:text-[#767676] duration-300 ease-in-out text-white hover:text-[gray]'>Journal</li>
-                        </ul>
+                        <div className=" w-3/4 z-50">
+                            <ul className={`lg:flex gap-x-[40px] lg:justify-center pt-[30px] lg:pt-[0] lg:static text-center font-sans] text-[20px] absolute  duration-700 ease-out ${show == true ? "bg-[#262626] w-[60%]  top-[50px] left-[0px] h-full" : "bg-[] top-[50px] h-full left-[-100px]"}`}>
+                                <li className='py-[5px] lg:py-[0px] font-bold  lg:hover:text-[#262626] lg:text-[#767676] duration-300 ease-in-out text-white hover:text-[gray]'>
+                                    <Link to="/">Home</Link>
+                                </li>
+                                <li className='py-[10px] lg:py-[0px] font-bold  lg:hover:text-[#262626] lg:text-[#767676] duration-300 ease-in-out text-white hover:text-[gray]'>
+                                    <Link to="/product">shop</Link>
+                                </li>
+                                <li className='py-[10px] lg:py-[0px] font-bold  lg:hover:text-[#262626] lg:text-[#767676] duration-300 ease-in-out text-white hover:text-[gray]'>About</li>
+                                <li className='py-[10px] lg:py-[0px] font-bold  lg:hover:text-[#262626] lg:text-[#767676] duration-300 ease-in-out text-white hover:text-[gray]'>Contacts</li>
+                                <li className='py-[10px] lg:py-[0px] font-bold  lg:hover:text-[#262626] lg:text-[#767676] duration-300 ease-in-out text-white hover:text-[gray]'>Journal</li>
+                            </ul>
+                        </div>
+                        <div ref={menulist} className="lg:hidden">
+                            {show == true ? < ImCross /> : <FaBars />}
+                        </div>
                     </div>
-                    <div className="lg:hidden" onClick={()=> setShow (!show)}>
-                        {show == true ?< ImCross /> :<FaBars/>}
-                    </div>
-                </div>
-            </Container>
+                </Container>
             </header>
         </>
 
